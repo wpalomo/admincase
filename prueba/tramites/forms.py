4afa0fae6e3c11345541ce372dc0baa83a431e71
Fernando <fernandoriquelme55@gmail.com>
2015-10-02 15:41:25 -0300

    nada

--- prueba/tramites/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Tramite
-from .models import RequisitosPresentados
-
-class RequisitoForm(forms.ModelForm):
-
-    class Meta:
-        model = RequisitosPresentados
-        fields = '__all__'
--- prueba/tramites/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import CreateView
-from .models import RequisitosPresentados
-from .forms import RequisitoForm
-# Create your views here.
-
-class RequisitoCreate(CreateView):
-    model = RequisitosPresentados
-    form_class = RequisitoForm
